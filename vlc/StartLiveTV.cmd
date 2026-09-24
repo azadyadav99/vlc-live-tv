809576d8ce1ba3ae5a02695d1582a4e232301061
AZAD YADAV <yadavjiazad@gmail.com>
2026-09-25 00:31:14 +0530

    fix: VLC path fallbacks (x86 + PATH)

--- vlc/StartLiveTV.cmd
+++ vlc/StartLiveTV.cmd
@@ -1,4 +1,7 @@
 @echo off
-start "" "C:\Program Files\VideoLAN\VLC\vlc.exe" --one-instance --started-from-file "%~dp0livetv.m3u8"
+set "VLC=C:\Program Files\VideoLAN\VLC\vlc.exe"
+if not exist "%VLC%" set "VLC=C:\Program Files (x86)\VideoLAN\VLC\vlc.exe"
+if not exist "%VLC%" for /f "delims=" %i in ('where vlc.exe 2^>nul') do set "VLC=%i"
+start "" "%VLC%" --one-instance --started-from-file "%~dp0livetv.m3u8"
 timeout /t 2 /nobreak >nul
 start "" "%~dp0ChannelMenu.html"
